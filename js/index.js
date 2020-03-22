943acd5ec53ef736fcc69810987e2ed2ddb3dd24
Prince <princeradebe@gmail.com>
2020-03-22 19:38:00 +0200

    Adding initial files

---
+++ js/index.js
@@ -0,0 +1,30 @@
+let confirmed = "loading...";
+let deaths = "loading...";
+let recovered = "loading...";
+let active = "loading...";
+
+let numberOfConfirmed = document.getElementById("confirmed");
+let numberOfDeaths = document.getElementById("deaths");
+let numberOfRecovered = document.getElementById("recovered");
+let numberOfActive = document.getElementById("active");
+
+numberOfConfirmed.innerHTML = `Confirmed: ${confirmed}`;
+numberOfRecovered.innerHTML = `Recovered: ${recovered}`;
+numberOfDeaths.innerHTML = `Deaths: ${deaths}`;
+// numberOfActive.innerHTML = `Active: ${active}`;
+
+const getCovidSA = async country => {
+    try {
+        const fetchCovid19SA = await fetch(`https://covid19.mathdro.id/api/countries/${country}`)
+        const data = await fetchCovid19SA.json();
+        numberOfConfirmed.innerHTML = `Confirmed: ${data.confirmed.value}`;
+        // numberOfActive.innerHTML = `Active: ${data.active.value}`;
+        numberOfRecovered.innerHTML = `Recovered: ${data.recovered.value}`;
+        numberOfDeaths.innerHTML = `Deaths: ${data.deaths.value}`;
+        return data;
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+getCovidSA("South Africa");
