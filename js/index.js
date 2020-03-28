bde72d8240063a7dd28e699916dc174ac5eefc4a
Prince <princeradebe@gmail.com>
2020-03-28 18:29:11 +0200

    New UI with API timestamp

--- js/index.js
+++ js/index.js
@@ -3,12 +3,14 @@ let deaths = "loading...";
 let recovered = "loading...";
 let critical = "loading...";
 let active = "loading...";
+let updatef = "loading...";
 
 let numberOfConfirmed = document.getElementById("confirmed");
 let numberOfDeaths = document.getElementById("deaths");
 let numberOfRecovered = document.getElementById("recovered");
 let numberOfCritical = document.getElementById("critical");
 let numberOfActive = document.getElementById('active');
+let updatedOn = document.getElementById('updated')
 
 numberOfConfirmed.innerHTML = `${confirmed}`;
 numberOfRecovered.innerHTML = `${recovered}`;
@@ -32,4 +34,16 @@ const getCovidSA = async country => {
     }
 }
 
+//Get the time when the API was last updated
+const getAll = async () => {
+    try {
+        const fetchCovidAll = await fetch("https://corona.lmao.ninja/all");
+        const data = await fetchCovidAll.json();
+        time = moment(data.updated).format("DD-MM-YYYY h:mm:ss");
+        updatedOn.innerHTML = `Last Updated: ${time}`;
+    } catch(err) {
+        console.log(err);
+    }
+}
 getCovidSA("South Africa");
+getAll();
