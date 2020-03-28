be8e330e4bedb1b6b2d7615ead1fedbe47e5c415
Prince <princeradebe@gmail.com>
2020-03-28 19:22:07 +0200

    Adds spinner when fecting data from API

--- js/index.js
+++ js/index.js
@@ -4,17 +4,27 @@ let numberOfDeaths = document.getElementById("deaths");
 let numberOfRecovered = document.getElementById("recovered");
 let numberOfCritical = document.getElementById("critical");
 let numberOfActive = document.getElementById('active');
-let updatedOn = document.getElementById('updated')
+let updatedOn = document.getElementById('updated');
 
 const getCovidSA = async country => {
     try {
         // const fetchCovid19SA = await fetch(`https://covid19.mathdro.id/api/countries/${country}`)
         const fetchCovid19SA = await fetch(`https://corona.lmao.ninja/countries/${country}`);
         const data = await fetchCovid19SA.json();
+
+        numberOfConfirmed.classList.remove("spinner");
         numberOfConfirmed.innerHTML = `${data.cases}`;
+
+        numberOfCritical.classList.remove("spinner");
         numberOfCritical.innerHTML = `${data.critical}`;
+
+        numberOfRecovered.classList.remove("spinner");
         numberOfRecovered.innerHTML = `${data.recovered}`;
+
+        numberOfDeaths.classList.remove("spinner");
         numberOfDeaths.innerHTML = `${data.deaths}`;
+
+        numberOfActive.classList.remove("spinner");
         numberOfActive.innerHTML =`${data.active}`;
         return data;
     } catch(err) {
