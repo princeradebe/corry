24d366645a4df0edc6a971b37c6bbdb82142006a
Prince <princeradebe@gmail.com>
2020-03-28 16:47:26 +0200

    Add pictures
    Adds new html structure
    
    Adds new CSS structure and font

--- js/index.js
+++ js/index.js
@@ -2,26 +2,30 @@ let confirmed = "loading...";
 let deaths = "loading...";
 let recovered = "loading...";
 let critical = "loading...";
+let active = "loading...";
 
 let numberOfConfirmed = document.getElementById("confirmed");
 let numberOfDeaths = document.getElementById("deaths");
 let numberOfRecovered = document.getElementById("recovered");
 let numberOfCritical = document.getElementById("critical");
+let numberOfActive = document.getElementById('active');
 
-numberOfConfirmed.innerHTML = `Confirmed: ${confirmed}`;
-numberOfRecovered.innerHTML = `Recovered: ${recovered}`;
-numberOfDeaths.innerHTML = `Deaths: ${deaths}`;
-numberOfCritical.innerHTML = `Critical: ${critical}`;
+numberOfConfirmed.innerHTML = `${confirmed}`;
+numberOfRecovered.innerHTML = `${recovered}`;
+numberOfDeaths.innerHTML = `${deaths}`;
+numberOfCritical.innerHTML = `${critical}`;
+numberOfActive.innerHTML = `${active}`;
 
 const getCovidSA = async country => {
     try {
         // const fetchCovid19SA = await fetch(`https://covid19.mathdro.id/api/countries/${country}`)
         const fetchCovid19SA = await fetch(`https://corona.lmao.ninja/countries/${country}`);
         const data = await fetchCovid19SA.json();
-        numberOfConfirmed.innerHTML = `Confirmed: ${data.cases}`;
-        numberOfCritical.innerHTML = `Critical: ${data.critical}`;
-        numberOfRecovered.innerHTML = `Recovered: ${data.recovered}`;
-        numberOfDeaths.innerHTML = `Deaths: ${data.deaths}`;
+        numberOfConfirmed.innerHTML = `${data.cases}`;
+        numberOfCritical.innerHTML = `${data.critical}`;
+        numberOfRecovered.innerHTML = `${data.recovered}`;
+        numberOfDeaths.innerHTML = `${data.deaths}`;
+        numberOfActive.innerHTML =`${data.active}`;
         return data;
     } catch(err) {
         console.log(err);
